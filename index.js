fe0f9a3ecbb54d9eeecc99258a771be1eb9e0df3
Siddarth Balaji <sid.balaji@berkeley.edu>
2023-01-10 11:38:48 -0500

    completed lab with all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    //return customerName
+    }
+
+//var bestCustomer
+function setBestCustomer(){
+    globalThis.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'asdf';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
